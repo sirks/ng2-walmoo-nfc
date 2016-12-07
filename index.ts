385544d23bae3387a86d997ccf96ea2011db42e1
Karina Pilyushonoka <k.pilyushonoka@gmail.com>
2016-12-07 12:15:33 +0200

    fix the way how to provide services

--- index.ts
+++ index.ts
@@ -1,18 +1,16 @@
 import { NgModule, ModuleWithProviders } from "@angular/core";
-import { HttpModule } from "@angular/http";
 import { WalmooNFCService } from "./src/walmoo-nfc.service";
 
-export * from "./src/walmoo-nfc.service";
-
 @NgModule({
-  imports: [ HttpModule ],
   providers: [{ provide: Window,  useValue: window }]
 })
 export class WalmooNFCModule {
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: WalmooNFCModule,
-      providers: [ WalmooNFCService ]
+      providers: [ WalmooNFCService, { provide: Window, useValue: window } ]
     };
   }
 }
+
+export { WalmooNFCService as WalmooService } from "./src/walmoo-nfc.service";
